4c2fd9e9433a174f3a6ed4c92374bf9a4fdd0923
panospd <anastapd@gmail.com>
2020-07-22 21:43:12 +0100

    Implemented backbone of ordering microservice

---
+++ src/Ordering/Ordering.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Base;
+
+namespace Ordering.Core.Repositories
+{
+    interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUsername(string username);
+    }
+}
---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int retry = 0)
+        {
+            int retryForAvailability = retry;
+
+            try
+            {
+                orderContext.Database.Migrate();
+
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                if (retry < 3)
+                {
+                    retryForAvailability++;
+
+                    var log = loggerFactory.CreateLogger<OrderContext>();
+                    log.LogError(e.Message);
+
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order
+                {
+                    Username = "swn",
+                    FirstName = "Mehmet",
+                    LastName = "Ozkaya",
+                    EmailAddress = "azozkme@gmail.com",
+                    AddressLine = "Bahcelievler",
+                    Country = "Turkey"
+                }
+            };
+        }
+    }
+}
